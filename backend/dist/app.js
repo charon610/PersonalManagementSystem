1c07c7ddc7f899a8116b014e6384ab826d2de3df
swpark <swpark@i2l.co.kr>
2023-06-20 17:44:18 +0900

    백엔드 MySQL 연동까지 성공

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -5,8 +5,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var path_1 = __importDefault(require("path"));
+var mysql_1 = __importDefault(require("mysql"));
 var app = (0, express_1.default)();
+var con = mysql_1.default.createConnection({
+    host: 'localhost',
+    user: 'admin',
+    password: 'Xptmxm1212!@',
+    port: 3306,
+    database: 'pmproject'
+});
+con.connect(function (err) {
+    if (err)
+        console.log("MySQL 연결 실패 : ", err);
+    else
+        console.log('Connected!');
+});
 app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
+app.get('/test', function (req, res) {
+    console.log('enter OK1 >>>>');
+    var sql = 'SELECT * FROM employee WHERE name=?';
+    con.query(sql, '강민수', function (err, results) {
+        if (err)
+            console.log(err);
+        else
+            res.send(results);
+    });
+});
 app.get('/', function (req, res) {
     res.sendFile(path_1.default.join(__dirname, '../public/index.html'));
 });
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,35 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import mysql from 'mysql';
 
 const app = express();
 
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'admin',
+    password: 'Xptmxm1212!@',
+    port: 3306,
+    database: 'pmproject'
+});
+
+con.connect(function(err){
+    if (err) console.log("MySQL 연결 실패 : ", err);
+    else console.log('Connected!');
+});
+
+
 app.use(express.static(path.join(__dirname, '../public')));
+
+// 테스트
+app.get('/test', (req, res) => {
+    console.log('enter OK1 >>>>');
+    let sql = 'SELECT * FROM employee WHERE name=?'
+    con.query(sql, '강민수', function (err, results) {
+        if (err) console.log(err);
+        else res.send(results);
+    });
+})
+
 app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
